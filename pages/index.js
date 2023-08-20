017b61a5932cae691c8b7cf7154ec592197a5fe3
buhe <bugu1986@126.com>
2023-08-20 19:22:26 +0800

    "Update the title of the SEO head component on the landing page to \"Bugu Apps\"."

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import SeoHead from "../components/SeoHead";
 export default function Home() {
   return (
     <>
-      <SeoHead title='Bugu Landing Page' />
+      <SeoHead title='Bugu Apps' />
       <Layout>
         <Hero />
         <Feature />
