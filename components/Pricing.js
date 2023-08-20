09b627cf5e22963e2099f238ac418e931e925d82
buhe <bugu1986@126.com>
2023-08-20 19:18:53 +0800

    "Add new components for Pagily, BilibiliSummary, and Writer with their respective images."

--- components/Pricing.js
+++ components/Pricing.js
@@ -320,6 +320,150 @@ const Pricing = () => {
               </motion.div>
             </ScrollAnimationWrapper>
           </div>
+          <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-3 gap-4 lg:gap-12 py-8 lg:py-12 px-6 sm:px-0 lg:px-6">
+            <ScrollAnimationWrapper className="flex justify-center">
+              <motion.div
+                variants={scrollAnimation}
+                className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-xl py-4 px-6 lg:px-12 xl:px-20"
+                whileHover={{
+                  scale: 1.1,
+                  transition: {
+                    duration: .2
+                  }
+                }}
+              >
+                <div className="p-4 lg:p-0 mt-6 lg:mt-16">
+                  <Image
+                    src="/assets/Pagily.png"
+                    width={100}
+                    height={100}
+                    alt="Pagily"
+                  />
+                </div>
+                <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7">
+                  Pagily
+                </p>
+                <ul className="flex flex-col list-inside pl-6 xl:pl-0 items-start justify-start text-left text-black-500 flex-grow">
+                  <li className="relative check custom-list my-2">
+                    Unlimited Bandwitch
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Encrypted Connection
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    No Traffic Logs
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Works on All Devices
+                  </li>
+                </ul>
+                {/* <div className="flex flex-col w-full justify-center mb-8 flex-none mt-12"> */}
+                {/* <p className="text-2xl text-black-600 text-center mb-4 ">
+                    Free
+                  </p> */}
+                {/* <ButtonOutline>Select</ButtonOutline>
+                </div> */}
+              </motion.div>
+            </ScrollAnimationWrapper>
+            <ScrollAnimationWrapper className="flex justify-center">
+              <motion.div
+                variants={scrollAnimation}
+                className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-xl py-4 px-6 lg:px-12 xl:px-20"
+                whileHover={{
+                  scale: 1.1,
+                  transition: {
+                    duration: .2
+                  }
+                }}
+              >
+                <div className="p-4 lg:p-0 mt-6 lg:mt-16">
+                  <Image
+                    src="/assets/BilibiliSummary.png"
+                    width={100}
+                    height={100}
+                    alt="BilibiliSummary"
+                  />
+                </div>
+                <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7">
+                  B 站 AI 总结{" "}
+                </p>
+                <ul className="flex flex-col list-inside pl-6 xl:pl-0 items-start justify-start text-left text-black-500 flex-grow">
+                  <li className="relative check custom-list my-2">
+                    Unlimited Bandwitch
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Encrypted Connection
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    No Traffic Logs
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Works on All Devices
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Connect Anyware{" "}
+                  </li>
+                </ul>
+                {/* <div className="flex flex-col w-full justify-center mb-8 flex-none mt-12"> */}
+                {/* <p className="text-2xl text-black-600 text-center mb-4 ">
+                  $9 <span className="text-black-500">/ mo</span>
+                </p> */}
+                {/* <ButtonOutline>Select</ButtonOutline>
+              </div> */}
+              </motion.div>
+            </ScrollAnimationWrapper>
+            <ScrollAnimationWrapper className="flex justify-center">
+              <motion.div
+                variants={scrollAnimation}
+                className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-xl py-4 px-6 lg:px-12 xl:px-20"
+                whileHover={{
+                  scale: 1.1,
+                  transition: {
+                    duration: .2
+                  }
+                }}
+              >
+                <div className="p-4 lg:p-0 mt-6 lg:mt-16">
+                  <Image
+                    src="/assets/Writer.png"
+                    width={100}
+                    height={100}
+                    alt="Writer"
+                  />
+                </div>
+                <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7">
+                  帮你写作文{" "}
+                </p>
+                <ul className="flex flex-col list-inside pl-6 xl:pl-0 items-start justify-start text-left text-black-500 flex-grow">
+                  <li className="relative check custom-list my-2">
+                    Unlimited Bandwitch
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Encrypted Connection
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    No Traffic Logs
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Works on All Devices
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Connect Anyware{" "}
+                  </li>
+                  <li className="relative check custom-list my-2">
+                    Get New Features{" "}
+                  </li>
+                </ul>
+                {/* <div className="flex flex-col w-full justify-center mb-8 flex-none mt-12">
+                <p className="text-2xl text-black-600 text-center mb-4 ">
+                  $12 <span className="text-black-500">/ mo</span>
+                </p>
+
+                <ButtonOutline>Select</ButtonOutline>
+              </div> */}
+              </motion.div>
+            </ScrollAnimationWrapper>
+          </div>
         </div>
         {/* <div className="flex flex-col w-full my-16"> */}
           {/* <ScrollAnimationWrapper>
