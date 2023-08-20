240d536591da4cabff6025c9f5a6a0e669aa74d0
buhe <bugu1986@126.com>
2023-08-20 16:42:05 +0800

    "Refactor Feature.js to include a hyperlink to the LangChain Swift GitHub repository in the heading."

--- components/Feature.js
+++ components/Feature.js
@@ -36,7 +36,9 @@ const Feature = () => {
 
         <motion.div className="flex flex-col items-end justify-center ml-auto w-full lg:w-9/12" variants={scrollAnimation}>
           <h3 className="text-3xl lg:text-4xl font-medium leading-relaxed text-black-600">
-            🐇 LangChain Swift
+              <a href="https://github.com/buhe/langchain-swift">
+                🐇 LangChain Swift 
+              </a>    
           </h3>
           <p className="my-2 text-black-500">
               A langchain copy, for ios or mac apps.
