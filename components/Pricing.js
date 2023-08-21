95738031f679c3dbd1fd3c7200acb9535739ce25
buhe <bugu1986@126.com>
2023-08-21 12:25:40 +0800

    remove useless pic

--- components/Pricing.js
+++ components/Pricing.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Testimoni from "./Testimoni";
 import ButtonPrimary from "./misc/ButtonPrimary";
 import ButtonOutline from "./misc/ButtonOutline.";
-import Maps from "../public/assets/HugeGlobal.svg";
+// import Maps from "../public/assets/HugeGlobal.svg";
 import { motion } from "framer-motion";
 import getScrollAnimation from "../utils/getScrollAnimation";
 import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
